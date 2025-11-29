ee6bd5d3370e752cbf09f49b6da82a5e0714c4f7
Hans <ruinb0w@qq.com>
2025-11-29 16:20:32 +0800

    add navigation icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
 import { Tabs } from 'expo-router';
-import { CircleCheck } from 'lucide-react-native';
+import { CircleCheck, CalendarDays, Store } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: 'Task', tabBarIcon: ({ color }) => <CircleCheck color={color} /> }} />
-      <Tabs.Screen name="calendar" options={{ title: 'Calendar' }} />
+      <Tabs.Screen name="calendar" options={{ title: 'Calendar', tabBarIcon: ({ color }) => <CalendarDays color={color} /> }} />
+      <Tabs.Screen name="shop" options={{ title: 'Store', tabBarIcon: ({ color }) => <Store color={color} /> }} />
       <Tabs.Screen name="about" options={{ title: 'About' }} />
     </Tabs>
   );
---
+++ app/(tabs)/shop.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+export default function Shop() {
+  return (
+    <Text>Shop</Text>
+  )
+}
